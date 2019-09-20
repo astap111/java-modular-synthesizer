e9b2ad0301f13acee19f4bc8e19b78dc9812fe3c
Astap <astap111@gmail.com>
2019-09-20 12:32:38 +0300

    keyboard panel added

---
+++ src/main/java/com/synthesizer/form/KeyboardPanel.java
@@ -0,0 +1,26 @@
+package com.synthesizer.form;
+
+import javax.swing.*;
+
+public class KeyboardPanel {
+    private JPanel panel1;
+    private JButton c1;
+    private JButton d1;
+    private JButton e1;
+    private JButton f1;
+    private JButton g1;
+    private JButton a1;
+    private JButton b1;
+    private JButton c2;
+    private JButton d2;
+    private JButton e2;
+    private JButton f2;
+    private JButton g2;
+    private JButton a2;
+    private JButton b2;
+    private JButton c3;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}

6056a10fa0317e80607f3a058c7fb278e0718912
Astap <astap111@gmail.com>
2019-09-23 23:57:42 +0300

    KeyboardKeyAdapter deleted as it's not the thing we need

--- src/main/java/com/synthesizer/form/KeyboardPanel.java
+++ src/main/java/com/synthesizer/form/KeyboardPanel.java
@@ -2,8 +2,6 @@ package com.synthesizer.form;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class KeyboardPanel extends JPanel {
     private WhiteKey[] whites = new WhiteKey[10];
@@ -48,17 +46,5 @@ public class KeyboardPanel extends JPanel {
             result[i++] = k;
         }
         return result;
-    }
-
-    class KeyboardKeyAdapter extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent e) {
-            super.keyPressed(e);
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            super.keyReleased(e);
-        }
     }
 }
